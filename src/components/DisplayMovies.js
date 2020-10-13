7050ffaa958c3d1940ec9615e3d0902e18ffeb9c
nartco <sebastien.thuillier@hotmail.com>
2020-10-13 02:28:06 +0200

    grid displayMovies

--- src/components/DisplayMovies.js
+++ src/components/DisplayMovies.js
@@ -1,20 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+import noPoster from "../assets/noposter.jpg";
 import "../css/displayMovies.css";
 
 const DisplayMovie = props => {
   const { movies } = props;
-  return movies.map(movie => (
-    <div className='displayMoviesContainer'>
-      <Link to='/movie'>
-        <h1 className='displayTitle'>{movie.title} </h1>
-        <img
-          src={"https://image.tmdb.org/t/p/w400/" + movie.poster_path}
-          alt={movie.original_title}
-          className='displayImage'
-        />
-      </Link>
-    </div>
+  console.log(movies);
+  return movies.map((movie, i) => (
+    <Grid item xs={12} sm={6} lg={3} key={i} spacing={'30px'}>
+      <div className='displayMoviesContainer'>
+        <Link to='/movie' style={{ textDecoration: "none" }}>
+          <div className='titleContainer'>
+            <h1 className='displayTitle'>{movie.title} </h1>
+          </div>
+          <img
+            src={
+              movie.poster_path
+                ? "https://image.tmdb.org/t/p/w400/" + movie.poster_path
+                : noPoster
+            }
+            alt={movie.original_title}
+            className='displayImage'
+          />
+          <div className='overlay'>
+            <div className='text'>
+              {movie.release_date.replaceAll("-", "/")}
+            </div>
+          </div>
+        </Link>
+      </div>
+    </Grid>
   ));
 };
 
